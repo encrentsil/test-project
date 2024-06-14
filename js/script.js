0254b4d69d55326865e4545b92ff661fd8feb132
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-06-14 11:24:07 +0000

    Test Project Body

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 4000,
       disableOnInteraction: false,
     },
     pagination: {
